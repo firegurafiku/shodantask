1beae6144daff127691a891d7046dfbd31a87ee2
Pavel Kretov <firegurafiku@gmail.com>
2017-06-19 12:41:44 +0300

    Implement URL extraction as a coroutine
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- main2.cpp
+++ main2.cpp
@@ -1,77 +1,30 @@
-#include <algorithm>
-#include <numeric>
-#include <iostream>
-#include <ios>
-#include <fstream>
-#include <regex>
-#include <boost/range/iterator_range.hpp>
-#include "MemoryMappedFile.hpp"
-
-#define BOOST_CB_DISABLE_DEBUG
-#include <boost/circular_buffer.hpp>
-
-template <class InputIt, class OutputIt>
-std::tuple<InputIt, OutputIt>
-copy_up_to(InputIt const& first, InputIt const& last, size_t count, OutputIt dest){
-    InputIt it = first;
-    for ( ; it != last && count != 0; --count, ++it) {
-        *dest = *it;
-        ++dest;
-    }
-    return {it, dest};
-}
-
-template <typename BidirIt>
-boost::iterator_range<std::regex_iterator<BidirIt>>
-regex_search_all(BidirIt const& first, BidirIt const& last, std::regex const& rex) {
-    using RegexIterator = std::regex_iterator<BidirIt>;
-    RegexIterator matchesBegin(first, last, rex);
-    RegexIterator matchesEnd {};
-    return { matchesBegin, matchesEnd };
-}
+#include <boost/numeric/conversion/cast.hpp>
+#include "BufferedUrlExtractor.hpp"
+#include "MMappedUrlExtractor.hpp"
 
 int main(int argc, char *argv[]) {
-    int n = 10;
-    std::string inputFn  = "input.txt";
-    std::string outputFn = "output.txt";
-    
-    constexpr auto urlRegexExpr = "https?://[a-z.]+(/[a-z.,/]*)?";
-    std::regex urlRegex(urlRegexExpr, std::regex::icase);
+    if (argc != 5)
+        return 1;
 
-    const size_t reasonableUrlLength = 40;
-    boost::circular_buffer<char> buf(100);
+    auto method   = std::string(argv[1]);
+    auto maxNum   = std::stoul(argv[2]);
+    auto inputFn  = std::string(argv[3]);
+    auto outputFn = std::string(argv[4]);
 
-    std::ifstream input(inputFn);
-    input.unsetf(std::ios_base::skipws);
-    auto fileIter = std::istream_iterator<char>(input);
-    auto fileEnd  = std::istream_iterator<char>();
+    decltype(&extractUrlsUsingCircularBuffer) extractorImpl =
+                        method == "mmap" ? nullptr
+                      : method == "buf"  ? extractUrlsUsingCircularBuffer
+                      : nullptr;
 
-    auto populate = [&fileIter, &fileEnd, &buf](size_t n) {
-        std::tie(fileIter, std::ignore) =
-                copy_up_to(fileIter, fileEnd, n, std::back_inserter(buf));
-    };
+    if (!extractorImpl)
+        return 1;
 
-    populate(buf.capacity());
-    while (1) {
-        auto lastMatchEnd = buf.end();
-        bool hadMatches = false;
-        for (auto const& match: regex_search_all(buf.begin(), buf.end(), urlRegex)) {
-            std::clog << "match\n";
-            std::cout << match.str() << std::endl;
-            lastMatchEnd = match[0].second;
-            hadMatches = true;
-        }
+    namespace coro = boost::coroutines2;
+    UrlExtractor::pull_type urlSeq(
+            coro::fixedsize_stack(),
+            [&](auto& yield) { return extractorImpl(inputFn, yield); });
 
-        if (fileIter == fileEnd)
-            break;
-
-        size_t margin = hadMatches
-                ? buf.end() - lastMatchEnd
-                : reasonableUrlLength;
-
-        populate(buf.capacity() - margin);
+    for (auto s: urlSeq) {
+        std::cout << s << std::endl;
     }
-
 }
-
-
